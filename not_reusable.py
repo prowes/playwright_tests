2965c62f542729a24a41d8c73867c3d9e29b6178
prowes <34424042+prowes@users.noreply.github.com>
2023-12-18 10:24:47 +0200

    Update not_reusable.py

--- not_reusable.py
+++ not_reusable.py
@@ -27,12 +27,9 @@ def test_website_after_search(page: Page):
     page.locator('id=nav-search-submit-text').click()
     page.locator('id=sp-cc-accept').click()
 
-    actual_text = page.locator('id=nav-link-accountList-nav-line-1').inner_text()
+    actual_text = page.locator('id=Qnav-link-accountList-nav-line-1').inner_text()
     expected_text = 'Hello, sign in'
     assert actual_text == expected_text, f'Received: {actual_text}'
 
-    sleep(5)
-    page.locator('id=s-result-sort-select').select_option('Price: Low to high')  # a price dropdown list
-    sleep(5)
-    shown_price = page.locator("xpath=//body/div[@id='a-page']/div[@id='search']/div[1]/div[1]/div[1]/span[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[2]/a[1]/span[1]/span[2]/span[3]").text_content()  # unfortunately XPath is an only option here :(
-    assert shown_price == "00"
+    #sleep(5)
+    page.wait_for_selector('id=s-result-sort-select')
